febf69eb4253ad3ed868b7567f47caf5c250801c
hkm <rbals0445@naver.com>
2022-03-16 22:53:53 +0900

    feat: 메인페이지 테스트

--- skkumeal-client/src/App.js
+++ skkumeal-client/src/App.js
@@ -1,10 +1,78 @@
+import styled from "styled-components";
+
 function App() {
-	console.log(process.env.REACT_APP_TEST_ENV);
+	//console.log(process.env.REACT_APP_TEST_ENV);
+	let img = ["/favicon.ico", "/favicon.ico", "/favicon.ico", "/favicon.ico"];
 	return (
-		<div>
-			<div style={{ textAlign: "center" }}>hello</div>
-		</div>
+		<Wrapper>
+			<Header>MyPage</Header>
+			<Logo>SKKU MEAL</Logo>
+			{/* 카테고리 버튼 */}
+			<CategoryWrapper>
+				<Category>
+					{img.map((elem, idx) => (
+						<img key={idx} src={elem} />
+					))}
+				</Category>
+			</CategoryWrapper>
+			{/* 검색바 */}
+			<SearchBarWrapper>
+				<SearchBar maxLength={30} />
+			</SearchBarWrapper>
+		</Wrapper>
 	);
 }
 
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: 0;
+	padding: 0;
+	background-color: rgb(141, 198, 63);
+	width: 100vw;
+	height: 100vh;
+`;
+
+const Header = styled.div`
+	display: flex;
+	margin-top: 40px;
+	margin-right: 40px;
+	font-size: 2rem;
+	justify-content: flex-end;
+`;
+
+const Logo = styled.div`
+	display: flex;
+	font-size: 100px;
+	color: #fff;
+	justify-content: center;
+`;
+
+const CategoryWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	margin: 20px;
+`;
+
+const Category = styled.div`
+	display: flex;
+	width: 767px;
+	justify-content: space-evenly;
+`;
+
+const SearchBarWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
+const SearchBar = styled.input`
+	display: flex;
+	width: 640px;
+	border-radius: 32px;
+	border-color: red;
+	min-height: 32px;
+	font-size: 16px;
+	justify-content: center;
+	padding-left: 16px;
+`;
 export default App;
